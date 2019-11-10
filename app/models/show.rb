7a316c031de41b7d468b6f71980045ba957b49f5
Kierra M <k.merriman32@hotmail.com>
2019-11-10 23:58:08 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,37 @@
 class Show < ActiveRecord::Base 
   
+def self.highest_rating 
+  self.maximum(:rating)
 end
+
+def self.most_popular_show 
+  #self.where(rating: highest_rating).first
+  #self.where(rating: highest_rating) returns aray but we want one instance thats why we have .first above
+  self.find_by(rating: highest_rating)
+end
+
+def self.lowest_rating 
+  self.minimum(:rating)
+end
+
+def self.least_popular_show 
+  self.find_by(rating: lowest_rating)
+end
+
+def self.ratings_sum 
+self.sum(:rating)
+end 
+
+def self.popular_shows
+  self.where("rating > 5")
+end 
+
+def self.shows_by_alphabetical_order
+  self.order("name ASC")
+end 
+
+end
+
+
+
+
