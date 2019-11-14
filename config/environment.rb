d47f71fdd67822918525dc942bb4a8069b48b60e
CleanSolLLC <mamark90@gmail.com>
2019-11-14 12:21:24 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@ require "bundler/setup"
 require 'yaml'
 require 'active_record'
 
+
+
 Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
@@ -15,3 +17,4 @@ DB = ActiveRecord::Base.establish_connection({
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
+
